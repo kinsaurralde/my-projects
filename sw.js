a9ed2484c3e5fc724d3f86b1349eb9619235ea9e
Kyle Insaurralde <kinsaurralde@users.noreply.github.com>
2018-07-27 14:29:13 -0700

    updated cache

--- sw.js
+++ sw.js
@@ -17,7 +17,7 @@
  *
  */
 
-const version = "0047"; // 07/25/2018 
+const version = "0048"; // 07/27/2018 
 const cacheName = `kyles-projects-${version}`;
 //console.log("Cache Name:",cacheName);
 //sessionStorage.setItem("version",cacheName);
