6d7762d1fafa9a20e6cf5ccf7df60fc4019ae566
Kyle Insaurralde <kinsaurralde@users.noreply.github.com>
2018-07-31 13:59:09 -0700

    tetris pre grid change

--- p5js/tetris/tetris.js
+++ p5js/tetris/tetris.js
@@ -34,14 +34,34 @@ function setupArrays() {
 }
 
 function setupRotate(old) {
-  console.log("Old:",old);
+  //console.log("Old:",old);
   for (i = 0; i < 3; i++) {
     for (j = 0; j < 3; j++) {
       rotateTemp[i][j] = 0;
     }
   }
   for (i = 0; i < 4; i++) {
-    rotateTemp[old[i][0]][old[i][1]] = 1;
+    rotateTemp[old[i][1]][old[i][0]] = 1;
+  }
+  //console.log("Step:",rotateTemp);
+  rotateTemp = rotateTemp.reverse();
+  for (i = 0; i < 3; i++) {
+    for (j = 0; j < i; j++) {
+      var temp = rotateTemp[i][j];
+      rotateTemp[i][j] = rotateTemp[j][i];
+      rotateTemp[j][i] = temp;
+    }
+  }
+  var k = 0;
+  for (i = 0; i < 3; i++) {
+    for (j = 0; j < 3; j++) {
+      if (rotateTemp[i][j] == 1) {
+        console.log(j,i);
+        activeShape.grid[k][0] = j;
+        activeShape.grid[k][1] = i;
+        k++;
+      }
+    }
   }
   console.log("New:",rotateTemp);
 }
@@ -212,7 +232,18 @@ function moveRight() {
 
 function rotateLeft() {
   if (activeShape.shape < 6) {
-    console.log("RL");
+    for (loop = 0; loop < 3; loop++) {
+      console.log("RL");
+      setupRotate(activeShape.grid);
+    }
+  } else {
+    console.log("cant rotate");
+  }
+}
+
+function rotateRight() {
+  if (activeShape.shape < 6) {
+    console.log("RR");
     setupRotate(activeShape.grid);
   } else {
     console.log("cant rotate");
@@ -317,6 +348,8 @@ class ActiveShape {
     for (var i = 0; i < 4; i++) {
       this.grid[i] = new Array(2);
     }
+
+    /*
     if (this.shape == 1) { // __-- // S Shape
       this.grid[0][0] = 0;
       this.grid[0][1] = 0;
@@ -414,6 +447,8 @@ class ActiveShape {
       this.g = 0;
       this.b = 0;
     }
+    
+    */
 
     this.drop = 0;
     this.moving = false;
@@ -452,7 +487,6 @@ class ActiveShape {
       fill(this.r, this.g, this.b);
       for (i = 0; i < 4; i++) {
         scaleRect(-270 + this.grid[i][0] * 60, -570 + this.grid[i][1] * 60 + this.drop, 58, 58, 0);
-        scaleText(i,-270 + this.grid[i][0] * 60, -570 + this.grid[i][1] * 60 + this.drop,);
       }
     }
   }
