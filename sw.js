6409f4560ef34c331b4f67a0483e32e810cd54a7
Kyle Insaurralde <kinsaurralde@users.noreply.github.com>
2018-08-02 14:45:34 -0700

    SENT TO MAIN SITE 8/2/18

--- sw.js
+++ sw.js
@@ -18,7 +18,7 @@
  */
 
  /* CHANGE MANIFEST VERSION ALSO */
-const version = "0054"; // 08/02/2018 
+const version = "0056"; // 08/02/2018 
 /* CHANGE MANIFEST VERSION ALSO */
 
 const cacheName = `kyles-projects-${version}`;
