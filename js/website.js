e8d4b8df0c6a03acd1fbb1c6818bcb7bf877cd8e
Kyle Insaurralde <kinsaurralde@users.noreply.github.com>
2018-08-03 14:59:47 -0700

    navbar layout changes

--- js/website.js
+++ js/website.js
@@ -1,6 +1,6 @@
 function navExpand() {
     var iframe = window.parent.document.getElementById("iframeover");
-    iframe.style.height = "320px"; 
+    iframe.style.height = "75vh"; 
 }       
 function navReset() {
     var iframe = window.parent.document.getElementById("iframeover");
