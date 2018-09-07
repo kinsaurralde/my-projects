0bb2c4a2292951ae049ab791b826104f7b73a8db
Kyle Insaurralde <kinsaurralde@users.noreply.github.com>
2018-09-06 20:17:59 -0700

    fix main title css

--- sw.js
+++ sw.js
@@ -18,7 +18,7 @@
  */
 
  /* CHANGE MANIFEST VERSION ALSO */
-const version = "0067"; // 09/5/2018 
+const version = "0068"; // 09/6/2018 
 /* CHANGE MANIFEST VERSION ALSO */
 
 const cacheName = `kyles-projects-${version}`;
