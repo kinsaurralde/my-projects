150a25c4039a1f119c8a5dd4f71ad93fd7a5823b
Kyle Insaurralde <kinsaurralde@users.noreply.github.com>
2018-10-18 14:11:12 -0700

    new site

---
+++ js/main.js
@@ -0,0 +1,8 @@
+function navExpand() {
+    var iframe = window.parent.document.getElementById("navbar-container");
+    iframe.style.height = "40vh"; 
+}       
+function navReset() {
+    var iframe = window.parent.document.getElementById("navbar-container");
+    iframe.style.height = "15vh"; 
+}
--- js/website.js
+++
@@ -1,8 +0,0 @@
-function navExpand() {
-    var iframe = window.parent.document.getElementById("iframeover");
-    iframe.style.height = "75vh"; 
-}       
-function navReset() {
-    var iframe = window.parent.document.getElementById("iframeover");
-    iframe.style.height = "98px"; 
-}
