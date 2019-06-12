b7df61bfe871b7980c0a48b3cf617008fe23f86c
Kyle Insaurralde <kinsaurralde@yahoo.com>
2019-06-12 14:34:00 -0700

    tinder data

---
+++ miniprojects/tinder/load.js
@@ -0,0 +1,7 @@
+var script = document.createElement('script');
+script.onload = function () {
+    console.log("loaded");
+};
+script.src = "file:///C:/Users/kinsa/OneDrive/Desktop/Files/Programming/js/tinder.js";
+
+document.head.appendChild(script); 
