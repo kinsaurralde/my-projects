984f1ef79dc9ab338a3b1797ef92402e51672c6c
Kyle Insaurralde <kinsaurralde@yahoo.com>
2019-07-27 12:07:27 -0700

    remove tinder miniproject

--- miniprojects/tinder/load.js
+++
@@ -1,7 +0,0 @@
-var script = document.createElement('script');
-script.onload = function () {
-    console.log("loaded");
-};
-script.src = "file:///C:/Users/kinsa/OneDrive/Desktop/Files/Programming/js/tinder.js";
-
-document.head.appendChild(script); 
