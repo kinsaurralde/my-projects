a962e6969fe146f48d66ac85613faea56f4f0282
Kyle Insaurralde <kinsaurralde@yahoo.com>
2019-07-28 17:04:51 -0700

    markdown to html

--- build/ws_281x.sh
+++ build/ws_281x.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
-# Convert README.md to HTML
-./markdown2.py README.md > readme.html
+# Convert README.md to HTML in github style
+./github_markdown.py ws_281x/README.md > ws_281x/temp_readme.html
+./add_github_css.py ws_281x/temp_readme.html > ws_281x/readme.html
+rm ws_281x/temp_readme.html
