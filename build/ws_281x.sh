5b0405c5c9e40180cf0892e537ff829586556db0
Kyle Insaurralde <kinsaurralde@yahoo.com>
2019-12-23 10:39:22 -0800

    fix build.sh

--- build/ws_281x.sh
+++ build/ws_281x.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 # Convert README.md to HTML in github style
-./github_markdown.py ws_281x/README.md > ws_281x/temp_readme.html
+./github_markdown.py ../projects/ws_281x/README.md > ws_281x/temp_readme.html
 ./add_github_css.py ws_281x/temp_readme.html > ws_281x/readme.html
 rm ws_281x/temp_readme.html
